329b330ff01f2e9dcb13fed70092770a5c6918d9
priya036 <priyadharshinis.21aid@kongu.edu>
2024-05-29 19:46:34 +0530

    deployment

--- Server/connect.js
+++ Server/connect.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 module.exports.connectdb = ()=>{
-    return mongoose.connect("mongodb://127.0.0.1:27017/Hall")
+    return mongoose.connect("mongodb+srv://priyadharshini:5799@cluster0.wtx7up8.mongodb.net/")
 }
+
 
 // mongodb://127.0.0.1:27017/Hall  --> it is for local connection
